f85ffc7e65f44b96ea27d618a00408d126cd78de
Adam Moisa <adammoisa@gmail.com>
2024-09-19 22:14:40 -0400

    fix deploy

--- components/onboarding/onboarding.tsx
+++ components/onboarding/onboarding.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createSiteProject, retrieveSites } from "@/actions/site";
+import { createSiteProject } from "@/actions/site";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,6 +14,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { retrieveSites } from "@/data/site";
 import { cn } from "@/lib/utils";
 import { WpSite } from "@/types";
 import { useAuth } from "@clerk/clerk-react";
