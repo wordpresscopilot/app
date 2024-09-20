d30225b1fb9e4e41e150ac42972a5f7547d3076e
Adam Moisa <adammoisa@gmail.com>
2024-09-19 20:08:11 -0400

    more details on plugin page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -5,7 +5,7 @@ export default function PublicLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex flex-col items-center flex-1 bg-muted/50">
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 md:gap-8">
         {children}
       </main>
     </div>
