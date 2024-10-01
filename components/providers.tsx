baba8fbb2e04206b22fb67f963ebd0ea281cf14a
Aaron Landy <aaronlan95@gmail.com>
2024-10-01 01:34:56 -0400

    Default theme system

--- components/providers.tsx
+++ components/providers.tsx
@@ -21,7 +21,7 @@ if (typeof window !== "undefined") {
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <PostHogProvider client={posthog}>
-      <NextThemesProvider {...props} defaultTheme="dark">
+      <NextThemesProvider {...props} defaultTheme="system">
         <SidebarProvider>
           <TooltipProvider>
             <SelectedSiteProvider>{children}</SelectedSiteProvider>
